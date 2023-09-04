7d29c27e9660c0c55082a648728d93114a346b1d
croutonp <cgpasaoa@gmail.com>
2023-09-03 22:29:04 -0400

    init

---
+++ GameDeveloper/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Attack LeftRightGoodnight = new("Left. Right. Goodnight.", 10);
+Attack GomuGomuNoPistol = new("Gomu Gomu no Pistol", 20);
+Attack Splash = new("Splash", 0);
+
+Enemy Kevin = new("Kevin");
+
+
+Kevin.AttackList.Add(LeftRightGoodnight);
+Kevin.AttackList.Add(GomuGomuNoPistol);
+Kevin.AttackList.Add(Splash);
+
+Kevin.RandomAttack();
